d9d4516c01ae2dfc730f34d559183d51b7f0874b
umidjon11111 <umidjongafforov844@gmail.com>
2025-11-09 21:16:01 +0500

    upt

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: {
+    origin: "https://sakura-frontend-rho.vercel.app",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
